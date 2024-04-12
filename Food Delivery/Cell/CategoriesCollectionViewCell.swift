2a57a3fc5e00209c6ff0a40282b15a55808de2b9
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2024-04-12 13:45:06 -0300

    PopularCategories
    Creating the categories view, the popular categories its working (without click)

---
+++ Food Delivery/Cell/CategoriesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  Food Delivery
+//
+//  Created by Gustavo Mendonca on 12/04/24.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconCategorie: UIImageView!
+}
--- Food Delivery/View/CategoriesViewController.swift
+++ Food Delivery/View/CategoriesViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
 
+    @IBOutlet weak var collectionViewCategories: UICollectionView!
+    
+    var images: [String] = ["imagePizza", "imageSalad", "imageBurguer","imagePizza", "imageSalad", "imageBurguer"]
+    //var bgIcons: [String] = ["BgIconPizza", "BgIconSalad", "BgIconBurguer", "BgIconPizza", "BgIconSalad", "BgIconBurguer"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,4 +36,22 @@ class CategoriesViewController: UIViewController {
     }
     */
 
+}
+
+extension CategoriesViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoriesCollectionViewCell
+        
+        cell.iconCategorie.image = UIImage(named: images[indexPath.row])
+       
+       // cell.back = UIColor(named: bgIcons[indexPath.row])
+        return cell
+    }
+    
+    
 }
