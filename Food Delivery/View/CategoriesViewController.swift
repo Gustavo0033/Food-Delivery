cc2c244439bb7ca1d0a787d71c2b6e07003622b2
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2024-04-12 13:01:31 -0300

    Initial

---
+++ Food Delivery/View/CategoriesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoriesViewController.swift
+//  Food Delivery
+//
+//  Created by Gustavo Mendonca on 12/04/24.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
